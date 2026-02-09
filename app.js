980524450df0c44a8fb4f1be2477a4f08c688595
prakhar-sonii <149144874+prakhar-sonii@users.noreply.github.com>
2026-02-09 20:03:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+let subjects = JSON.parse(localStorage.getItem("subjects")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(sec => sec.classList.add('hidden'));
+  document.getElementById(id).classList.remove('hidden');
+}
+
+function addSubject() {
+  const name = document.getElementById("subjectName").value;
+  const priority = document.getElementById("priority").value;
+
+  subjects.push({ name, priority });
+  localStorage.setItem("subjects", JSON.stringify(subjects));
+  renderSubjects();
+}
+
+function renderSubjects() {
+  const list = document.getElementById("subjectList");
+  list.innerHTML = "";
+  subjects.forEach((s, i) => {
+    list.innerHTML += `<li>${s.name} (${s.priority})
+      <button onclick="deleteSubject(${i})">❌</button></li>`;
+  });
+}
+
+function deleteSubject(i) {
+  subjects.splice(i, 1);
+  localStorage.setItem("subjects", JSON.stringify(subjects));
+  renderSubjects();
+}
+
+function addTask() {
+  const name = taskName.value;
+  const deadline = document.getElementById("deadline").value;
+
+  tasks.push({ name, deadline, done: false });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+}
+
+function resetData() {
+  localStorage.clear();
+  location.reload();
+}
+
+renderSubjects();
